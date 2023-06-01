c93a3ba1d8608b8d37422971a5f5d2d2678db73a
Laura Eck <me@lauraeck.com>
2023-06-01 15:42:52 +0200

    Exercise 1.1

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,53 @@
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  )
+}
+
+const Content = (props) => {
+  let result = props.parts.map((element, index) => {
+    return(
+      <p key={'part'+ index}>
+        {element.name} {element.exercises}
+      </p>
+    )
+  });
+
+  return (
+      <>
+        {result}
+      </>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <>
+      <p>Number of exercises {props.exercises.reduce((a, b) => a + b)}</p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={
+        [{name: part1, exercises: exercises1}, {name: part2, exercises: exercises2}, {name: part3, exercises: exercises3}]
+      }/>
+      <Total exercises={[exercises1, exercises2, exercises3]}/>
+    </div>
+  )
+}
+
+export default App
